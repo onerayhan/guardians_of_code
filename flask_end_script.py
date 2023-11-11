31293bd94a46878152e202e00c3195a3b29c3395
Hamza Özdemir <124764891+competetor@users.noreply.github.com>
2023-11-11 18:11:35 +0300

    Update flask_end_script.py
    SQL modelleri eklendi + follow_system endpointleri tamamlandı

--- flask_end_script.py
+++ flask_end_script.py
@@ -5,28 +5,56 @@ from datetime import datetime, timedelta
 from flask_cors import CORS
 from functools import wraps
 import uuid
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
 
-users = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:admin@127.0.0.1/cs308_backend_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+class Users(db.Model):
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    birthday = db.Column(db.String(100), nullable=False)
+    profile_picture = db.Column(db.LargeBinary, nullable=True)
+    public_id = db.Column(db.String(32), primary_key=True, nullable=False)
+    
+class FollowSystem(db.Model):
+    follower_username = db.Column(db.String(100), nullable=False, primary_key=True)
+    followed_username = db.Column(db.String(100), nullable=False, primary_key=True)
+    __table_args__ = (
+        db.ForeignKeyConstraint(['follower_username'], ['users.username']),
+        db.ForeignKeyConstraint(['followed_username'], ['users.username'])
+    )
+
+#burada problem var
 app.config['SECRET_KEY'] = '5p#r7GZ9!J^T8&d@E*2$yS'
 
 def generate_public_id():
     return str(uuid.uuid4().hex)
 
 @app.route('/api/register', methods=['POST'])
-
 def register():
     data = request.get_json()
-    username = data.get('username')
+    username = data.get('username')    
     password = data.get('password')
-    if not username or not password:
-        return jsonify({'message': 'Both username and password are required'}), 400
+    email = data.get('email')
+    birthday = data.get('birthday')    
+    if not email or not password or not birthday or not username:
+        return jsonify({'message': 'Email/Password/Birthday/Username are required'}), 400
+    
+    if Users.query.filter_by(username=username).first():
+        return jsonify({'message': 'Such username already exists'}), 409  
+    
+    if Users.query.filter_by(email=email).first():
+        return jsonify({'message': 'Such email already exists'}), 409          
 
     public_id = generate_public_id()
-    users.append({'username': username, 'password': password, 'public_id' : public_id})
+    db.session.add(Users(username=username, email=email, password=password, birthday=birthday, public_id=public_id))
+    db.session.commit()
     return jsonify({'message': 'User registered successfully', 'public_id': public_id}), 201
 
 @app.route('/api/login', methods=['POST'])
@@ -34,30 +62,93 @@ def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')  
-      
+    
     if not username or not password:
-        return jsonify({'message' : "Both username and password are required"}), 400
+        return jsonify({'message' : "Both email and password are required"}), 400
     
-    user_checker = False
-    public_id_login = 0
-    for user in users:
-        if user['username'] == username and user['password'] == password:
-            user_checker = True
-            public_id_login = user['public_id']
+    user = Users.query.filter_by(username=username).first()
+    
+    if user and user.password == password:        
+        token = jwt.encode({
+            'public_id': user.public_id,
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        }, app.config['SECRET_KEY'], algorithm='HS256')
 
-    if user_checker == False:
-        return jsonify({'message': 'Invalid username or password'}), 401
+        return jsonify({'message': 'User login successful', 'token': token}), 200
+    else:
+        return jsonify({'message': 'Invalid username or password '}), 401
+    
+@app.route('/api/user_info', methods=['POST'])
+def get_user_follower_following():
+    data = request.get_json()
+    username = data.get('username')
+    
+    user = Users.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'message': 'Invalid username'}), 404
+    
+    followers_query = db.session.query(FollowSystem.follower_username).filter_by(followed_username=user.username)
+    followers = [follower[0] for follower in followers_query.all()]
 
-    token = jwt.encode({
-        'public_id': public_id_login,
-        'exp': datetime.utcnow() + timedelta(minutes=30)
-    }, app.config['SECRET_KEY'], algorithm='HS256')    
+
+    followed_query = db.session.query(FollowSystem.followed_username).filter_by(follower_username=user.username)
+    followed = [followed_user[0] for followed_user in followed_query.all()]
     
-    return jsonify({'token': token}, {'message' : 'User login succesfull'}), 200    
+    return jsonify({f'followers for {username}': followers, f'followed for {username}': followed})   
     
+@app.route('/api/follow', methods=['POST'])
+def follow_user():
+    data = request.get_json()
+    follower_username = data.get('follower_username')
+    followed_username = data.get('followed_username')
+    
+    follower = Users.query.filter_by(username=follower_username).first()
+    followed = Users.query.filter_by(username=followed_username).first()
+
+    if not follower or not followed:
+        return jsonify({'message': 'Invalid usernames'}), 404
+    
+    existing_relationship = FollowSystem.query.filter_by(follower_username=follower.username, followed_username=followed.username).first()
+    if existing_relationship:
+        return jsonify({'message': 'Relationship already exists'}), 400
+    
+    new_relationship = FollowSystem(follower_username=follower.username, followed_username=followed.username)
+    db.session.add(new_relationship)
+    db.session.commit()
+
+    return jsonify({'message': 'Relationship added successfully'}), 201
+
+@app.route('/api/unfollow', methods=['POST'])
+def unfollow_user():
+    data = request.get_json()
+    follower_username = data.get('follower_username')
+    followed_username = data.get('followed_username')
+
+    
+    follower = Users.query.filter_by(username=follower_username).first()
+    followed = Users.query.filter_by(username=followed_username).first()
+
+    if not follower or not followed:
+        return jsonify({'message': 'Invalid usernames'}), 404
+    
+    existing_relationship = FollowSystem.query.filter_by(follower_username=follower.username, followed_username=followed.username).first()
+    if not existing_relationship:
+        return jsonify({'message': 'Relationship does not exist'}), 404
+    
+    db.session.delete(existing_relationship)
+    db.session.commit()
+
+    return jsonify({'message': 'Relationship removed successfully'}), 200
+   
 @app.route('/api/get_all_users', methods=['GET'])
 def get_all_users():
-    return jsonify(users)
+    all_users = Users.query.all()    
+    users_list = [
+        {'username': user.username, 'password': user.password, 'email': user.email, 'birthday': user.birthday, 'profile_picture': user.profile_picture, 'public_id': user.public_id}
+        for user in all_users
+    ]
+    
+    return jsonify(users_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
