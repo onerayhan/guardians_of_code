77b20a2e86de412a5d937006b8ce555d798f5be0
Hamza Özdemir <124764891+competetor@users.noreply.github.com>
2023-11-13 10:59:26 +0300

    Update flask_end_script.py
    added delete user & change password endpoints; auto login db model and auto login endpoint current present but not deployed;
    if deployed /login will be changed accordingly

--- flask_end_script.py
+++ flask_end_script.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request
-from  werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from datetime import datetime, timedelta
 from flask_cors import CORS
@@ -14,7 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:admin@127.0
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Users(db.Model):
+class users(db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
@@ -30,11 +29,25 @@ class FollowSystem(db.Model):
         db.ForeignKeyConstraint(['followed_username'], ['users.username'])
     )
 
+#class DeviceToken(db.Model):
+#   device_token = db.Column(db.String(255), primary_key=True, nullable=False)
+#   public_id = db.Column(db.String(32), nullable=False)
+#   __table_args__ = (
+#        db.ForeignKeyConstraint(['public_id'], ['users.public_id'])        
+#    )
+
 #burada problem var
 app.config['SECRET_KEY'] = '5p#r7GZ9!J^T8&d@E*2$yS'
 
 def generate_public_id():
     return str(uuid.uuid4().hex)
+
+def check_password(username, password):
+    user = users.query.filter_by(username=username).first()
+    if user.password == password:
+        return True
+    else:
+        return False
 
 @app.route('/api/register', methods=['POST'])
 def register():
@@ -46,14 +59,14 @@ def register():
     if not email or not password or not birthday or not username:
         return jsonify({'message': 'Email/Password/Birthday/Username are required'}), 400
     
-    if Users.query.filter_by(username=username).first():
+    if users.query.filter_by(username=username).first():
         return jsonify({'message': 'Such username already exists'}), 409  
     
-    if Users.query.filter_by(email=email).first():
+    if users.query.filter_by(email=email).first():
         return jsonify({'message': 'Such email already exists'}), 409          
 
     public_id = generate_public_id()
-    db.session.add(Users(username=username, email=email, password=password, birthday=birthday, public_id=public_id))
+    db.session.add(users(username=username, email=email, password=password, birthday=birthday, public_id=public_id))
     db.session.commit()
     return jsonify({'message': 'User registered successfully', 'public_id': public_id}), 201
 
@@ -62,28 +75,49 @@ def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')  
+    #device_token = data.get('device_token')
     
     if not username or not password:
         return jsonify({'message' : "Both email and password are required"}), 400
     
-    user = Users.query.filter_by(username=username).first()
+    user = users.query.filter_by(username=username).first()
     
     if user and user.password == password:        
         token = jwt.encode({
             'public_id': user.public_id,
             'exp': datetime.utcnow() + timedelta(minutes=30)
         }, app.config['SECRET_KEY'], algorithm='HS256')
+        
+        #existing_device_token = DeviceToken.query.filter_by(device_token=device_token).first()
+        
+        #if not existing_device_token:
+        #    db.session.add(DeviceToken(device_token=device_token, public_id=user.public_id))
+        #    db.session.commit()
 
         return jsonify({'message': 'User login successful', 'token': token}), 200
     else:
         return jsonify({'message': 'Invalid username or password '}), 401
+
+@app.route('/api/user_delete', methods=['POST']) 
+def delete_user():
+    data = request.get_json()
+    username = data.get('username')
+    
+    user = users.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({'message': 'Invalid username'}), 404
+    
+    db.session.delete(user)
+    db.session.commit()
+    
+    return jsonify({'message': 'User successfully deleted'}), 200        
     
 @app.route('/api/user_info', methods=['POST'])
 def get_user_follower_following():
     data = request.get_json()
     username = data.get('username')
     
-    user = Users.query.filter_by(username=username).first()
+    user = users.query.filter_by(username=username).first()
     if not user:
         return jsonify({'message': 'Invalid username'}), 404
     
@@ -94,7 +128,27 @@ def get_user_follower_following():
     followed_query = db.session.query(FollowSystem.followed_username).filter_by(follower_username=user.username)
     followed = [followed_user[0] for followed_user in followed_query.all()]
     
-    return jsonify({f'followers for {username}': followers, f'followed for {username}': followed})   
+    return jsonify({f'followers for {username}': followers, f'followed for {username}': followed})
+
+@app.route('/api/change_password', methods=['POST'])
+def change_password():
+    data = request.get_json()
+    username = data.get('username')
+    old_password = data.get('old_password')
+    new_password = data.get('new_password')
+
+    if not username or not old_password or not new_password:
+        return jsonify({'message': 'Username/old password/new password are required'}), 400
+
+    user = users.query.filter_by(username=username).first()
+
+    if not user or not check_password(username, old_password):
+        return jsonify({'message': 'Invalid username or old password'}), 401
+    
+    user.password = new_password
+    db.session.commit()
+
+    return jsonify({'message': 'Password changed successfully'}), 200 
     
 @app.route('/api/follow', methods=['POST'])
 def follow_user():
@@ -102,8 +156,8 @@ def follow_user():
     follower_username = data.get('follower_username')
     followed_username = data.get('followed_username')
     
-    follower = Users.query.filter_by(username=follower_username).first()
-    followed = Users.query.filter_by(username=followed_username).first()
+    follower = users.query.filter_by(username=follower_username).first()
+    followed = users.query.filter_by(username=followed_username).first()
 
     if not follower or not followed:
         return jsonify({'message': 'Invalid usernames'}), 404
@@ -125,8 +179,8 @@ def unfollow_user():
     followed_username = data.get('followed_username')
 
     
-    follower = Users.query.filter_by(username=follower_username).first()
-    followed = Users.query.filter_by(username=followed_username).first()
+    follower = users.query.filter_by(username=follower_username).first()
+    followed = users.query.filter_by(username=followed_username).first()
 
     if not follower or not followed:
         return jsonify({'message': 'Invalid usernames'}), 404
@@ -139,10 +193,25 @@ def unfollow_user():
     db.session.commit()
 
     return jsonify({'message': 'Relationship removed successfully'}), 200
+
+#@app.route('/api/auto_login', methods=['POST'])
+#def auto_login():
+#    data = request.get_json()
+#    device_token = data.get('device_token')
+#     
+#    existing_token = DeviceToken.query.filter_by(device_token=device_token).first()
+#    token_pid = existing_token.public_id
+#    user = users.query.filter_by(public_id=public_id).first()
+#    public_id = user.public_id
+#
+#    if user:
+#        return jsonify({'public_id': user.public_id}), 200
+#    else:
+#        return jsonify({'message': 'User not found'}), 404
    
 @app.route('/api/get_all_users', methods=['GET'])
 def get_all_users():
-    all_users = Users.query.all()    
+    all_users = users.query.all()    
     users_list = [
         {'username': user.username, 'password': user.password, 'email': user.email, 'birthday': user.birthday, 'profile_picture': user.profile_picture, 'public_id': user.public_id}
         for user in all_users
